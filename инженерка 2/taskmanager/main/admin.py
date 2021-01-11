add2995b9befb6f5682f256ae81d38d59a3a84ee
MaryRibb <rybnikovamary67@gmail.com>
2021-01-11 11:41:21 +0300

    injer224

--- инженерка 2/taskmanager/main/admin.py
+++ инженерка 2/taskmanager/main/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 
+
+
+
 # Register your models here.
 from .models import Booking
 from .models import Theclient
@@ -24,13 +27,10 @@ admin.site.register(TypeOfRooms)
 admin.site.register(Room)
 
 
-def make_published(request, queryset):
-    queryset.update(status='p')
-    make_published.short_description = "Mark selected stories as published"
+class QuestionAdminSite(admin.ModelAdmin):
+    model = Booking
+    fields =['typebooking', 'timeofbooking']
 
 
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status']
-    ordering = ['title']
-    actions = [make_published]
+admin.site.register(Booking, QuestionAdminSite)
 
